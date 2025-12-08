03e129f187b4158fd6d5317cb3978bbca09fc4e0
Wellington Umbelino <wellington.umbelino@kaspper.com.br>
2025-12-07 22:16:32 -0300

    create api config

---
+++ src/api/apiConfig.js
@@ -0,0 +1,10 @@
+const { default: axios } = require('axios')
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export default api
